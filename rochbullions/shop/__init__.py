64981f6f1bbf967e61aeda95f0d13bfd14420871
madina-lab <153171520+madina-lab@users.noreply.github.com>
2023-12-09 13:11:58 +0000

    udate deleted old files

--- rochbullions/shop/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rochbul.db'
-db = SQLAlchemy(app)
-
-from shop import routes
